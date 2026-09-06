793966799ab8c2fa6051db5d4e37df70904f6c03
PTOM76 <ptms76@outlook.jp>
2026-09-06 18:27:35 +0900

    chree_accountsのテーブルをつくるマイグレ

---
+++ database/migrations/2026_09_05_154948_create_chree_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// php artisan make:migration create_chree_accounts_table でつくったやつー
+// php artisan migrate でマイグレーションできる（こいつはたぶんテーブルつくるんやと思う）
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('chree_accounts', function (Blueprint $table): void {
+            // $table->id();
+            $table->ulid('id')->primary(); // 主キー: id (ulid)
+            $table->string('email')->unique()->nullable(); // メールアドレス
+            $table->timestamp('email_verified_at')->nullable(); // 認証日時
+            $table->string('display_name')->nullable(); // 表示名
+            $table->timestamp('suspended_at')->nullable(); // 停止日時
+            $table->timestamp('deleted_at')->nullable(); // 削除日時
+            $table->timestamps(); // 作成日時/更新日時
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('chree_accounts');
+    }
+};
