7067bfaedb814a86251c6570a7284357ac5687c5
PTOM76 <ptms76@outlook.jp>
2026-09-12 21:57:05 +0900

    a

--- app/Modules/Credential/Http/PasskeyController.php
+++ app/Modules/Credential/Http/PasskeyController.php
@@ -66,10 +66,19 @@ logger()->debug('options inspect', [
 ]);
         
         // わかったことはこのL63の行を消すとログアウトされなくなる
-        // $request->session()->put(self::PENDING_OPTIONS, serialize($options));
+
+        logger()->warning('BEFORE PUT', [
+    'id' => $request->session()->getId(),
+]);
+
+$request->session()->put(self::PENDING_OPTIONS, $options);
+
+logger()->warning('AFTER PUT', [
+    'id' => $request->session()->getId(),
+]);
 
         // これはいける、だからserialize(..)
-        // $request->session()->put(self::PENDING_OPTIONS, ['aaa' => 'aaa']);
+        $request->session()->put(self::PENDING_OPTIONS, ['aaa' => 'aaa']);
 
         // 次の往復と突き合わせるために、成功した側も残す
         $this->diagnostics->reportStep('options', $request);
